79551fbaa6fcbdf1f6552b2aaa72c1ab02d51201
Erik Cohen <ejcohe22@colby.edu>
2022-02-03 21:22:33 -0500

    Clear

--- src/proj1CohenCorrellDampteyDimitrov/Main.java
+++ src/proj1CohenCorrellDampteyDimitrov/Main.java
@@ -13,76 +13,13 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-//menu imports
-import java.util.AbstractMap.SimpleEntry;
-import java.util.Map.Entry;
 import javafx.scene.control.*;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.Effect;
-import javafx.scene.effect.Glow;
-import javafx.scene.effect.SepiaTone;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import java.util.Optional;
 
 public class Main extends Application{
-    /**
-     * Creates a Menubar for the Application
-     * 1 Menu item: File
-     * has "exit" and "clear" actions
-     * @return      MenuBar
-     * @see        Menu
-    */
-    private MenuBar createMenu(){
-        MenuBar menuBar = new MenuBar();
-        Menu menuFile = new Menu("File");
-        menuBar.getMenus().addAll(menuFile);
-
-        MenuItem clear = new MenuItem("Clear");
-        clear.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent t) {
-                //stage.setVisible(false);
-            }
-        });
-
-        MenuItem exit = new MenuItem("Exit");
-        exit.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent t) {
-                System.exit(0);
-            }
-        });
-        menuFile.getItems().addAll(clear, new SeparatorMenuItem(), exit);
-        return menuBar;
-    }
-
-    /**
-     * Creates a Button for the Application
-     * Button stuff
-     * button click events
-     * @return      Button
-     * @see        Button
-    */
-    private Button createButton(String message){
-        Button btn = new Button();
-        btn.setText(message);
-        return btn;
-    }
-	/**
-     * Creates a Button for the Application
-     * Button stuff
-     * button click events
-     * @return      Button
-     * @see        Button
-    */
-	
-    private ToolBar createToolBar(){
-		ToolBar toolBar = new ToolBar();
-        return toolBar;
-    }
     /**
      * Creates Two Buttons and adds to "Stage"
      * Shows stage in window and handles button "click" events
@@ -98,67 +35,52 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) {
         Scene scene = new Scene(new VBox(), 400, 350);
+        MenuBar menuBar = new MenuBar();
+        Menu menuFile = new Menu("File");
+        menuBar.getMenus().addAll(menuFile);
+        MenuItem clear = new MenuItem("Clear");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                System.exit(0);
+            }
+        });
+        menuFile.getItems().addAll(clear, new SeparatorMenuItem(), exit);
 
-        MenuBar menuBar = createMenu();
-        ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
 
-		ToolBar toolBar = createToolBar();
-        Button hello = createButton("Hello");
+		ToolBar toolBar = new ToolBar();
+        Button hello = new Button();
+        hello.setText("Hello");
 		hello.setStyle("-fx-background-color: #90EE90; -fx-border-radius: .25em; -fx-background-radius: .25em; -fx-border-color: #000000;");
-		Button goodbye = createButton("Goodbye");
-		
-//		goodbye.setOnAction(new EventHandler<ActionEvent>() {
-//			 
-//            /**
-//             * Returns an Image object that can then be painted on the screen. 
-//             * The url argument must specify an absolute <a href="#{@link}">{@link URL}</a>. The name
-//             * argument is a specifier that is relative to the url argument. 
-//             * <p>
-//             * This method always returns immediately, whether or not the 
-//             * image exists. When this applet attempts to draw the image on
-//             * the screen, the data will be loaded. The graphics primitives 
-//             * that draw the image will incrementally paint on the screen. 
-//             *
-//             * @param  event the location of the image, relative to the url argument
-//             * @return      the image at the specified URL
-//             * @see        Image
-//            */
-//        @Override
-//        public void handle(ActionEvent event) {
-//        	
-//            //System.out.println(message);
-//        	
-//        	textField.appendText("Goodbye");
-//        	
-//        }
-//    });//btn.setOnAction
+		Button goodbye = new Button();
+        goodbye.setText("Goodbye");
 		
 		goodbye.setStyle("-fx-background-color: #FFC0CB; -fx-border-radius: .25em; -fx-background-radius: .25em;  -fx-border-color: #000000;");
 		toolBar.getItems().add(hello);
 		toolBar.getItems().add(goodbye);
-        ((VBox) scene.getRoot()).getChildren().addAll(toolBar);
 
 		TextField textField = new TextField("Sample text");
 		textField.setPrefHeight(290);
 		textField.setAlignment(Pos.TOP_LEFT);
+
+        clear.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                textField.setText("Sample text");
+                hello.setText("Hello");
+            }
+        });
+
+        ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
+        ((VBox) scene.getRoot()).getChildren().addAll(toolBar);
         ((VBox) scene.getRoot()).getChildren().addAll(textField);
         
         goodbye.setOnAction(new EventHandler<ActionEvent>() {
-			 
-            /**
-             * Returns an Image object that can then be painted on the screen. 
-             * The url argument must specify an absolute <a href="#{@link}">{@link URL}</a>. The name
-             * argument is a specifier that is relative to the url argument. 
-             * <p>
-             * This method always returns immediately, whether or not the 
-             * image exists. When this applet attempts to draw the image on
-             * the screen, the data will be loaded. The graphics primitives 
-             * that draw the image will incrementally paint on the screen. 
-             *
-             * @param  event the location of the image, relative to the url argument
-             * @return      the image at the specified URL
-             * @see        Image
-            */
+        /**
+         * Appends "Goodbye" to text field on click
+         *
+         * @param  event event created on button click
+         * @return      
+        */
         @Override
         public void handle(ActionEvent event) {
         	
@@ -172,30 +94,17 @@ public class Main extends Application{
         hello.setOnAction(new EventHandler<ActionEvent>() {
 			 
             /**
-             * Returns an Image object that can then be painted on the screen. 
-             * The url argument must specify an absolute <a href="#{@link}">{@link URL}</a>. The name
-             * argument is a specifier that is relative to the url argument. 
-             * <p>
-             * This method always returns immediately, whether or not the 
-             * image exists. When this applet attempts to draw the image on
-             * the screen, the data will be loaded. The graphics primitives 
-             * that draw the image will incrementally paint on the screen. 
+             * Opens Dialog Box to change button text
              *
-             * @param  event the location of the image, relative to the url argument
-             * @return      the image at the specified URL
-             * @see        Image
+             * @param  event event created on button press
+             * @return
             */
         @Override
         public void handle(ActionEvent event) {
-        	
-            //System.out.println(message);
-        	
         	TextInputDialog dialog = new TextInputDialog();
         	dialog.setTitle("Give me a number");
         	dialog.setHeaderText("Give me an integer from 0 to 255:");
-        	
         	Optional<String> result = dialog.showAndWait();
-        	
         	result.ifPresent(number -> {hello.setText(number);});
         	
         }
